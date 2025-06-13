619f7c6fda7f1fed989fcc4017c785232809fb1c
groverongo <ugartegrover@gmail.com>
2025-06-12 23:04:21 -0500

    feat: implement match data retrieval and schema for FIFA Club World Cup poller

---
+++ src/constant/urls.ts
@@ -0,0 +1,3 @@
+export const SCORE_GROUP_PHASE = 'https://prod-cdn-public-api.livescore.com/v1/api/app/stage/soccer/intl/fifa-club-world-cup-group-${groupLetter}/-5?locale=en&MD=1';
+
+export const SCORE_MATCHES_DATE = 'https://prod-cdn-mev-api.livescore.com/v1/api/app/date/soccer/${date}/-5?locale=en&MD=1';
---
+++ src/schemas/poller.ts
@@ -0,0 +1,63 @@
+export interface CompetitionData {
+    Stages: Stage[];
+}
+
+export interface Stage {
+    Sid:         string;
+    Snm:         string;
+    Scd:         string;
+    GroupLetter?: string;
+    hasDraw:     boolean;
+    Events:      Event[];
+}
+
+export interface Event {
+    Eid:         string;
+    Tr1:         string; // Team 1 goals in 90 minutes
+    Tr2:         string; // Team 2 goals in 90 minutes
+    T1:          T1[];
+    T2:          T1[];
+    Eps:         Eps;
+    ErnInf:      string;
+    Esid:        number;
+    Epr:         number;
+    Ecov:        number;
+    Esd:         number;
+    LuUT:        number;
+    Edf:         number;
+    EO:          number;
+    EOX:         number;
+    Ehid:        number;
+    seriesInfo?: SeriesInfo;
+    Awt?:        number;
+    Tr1ET?:      string; // Team 1 goals in extra time
+    Tr2ET?:      string; // Team 2 goals in extra time
+    Trp1?:       string; // Team 1 goals in penalties
+    Trp2?:       string; // Team 2 goals in penalties
+}
+
+export enum Eps {
+    Aet = "AET",
+    Ap = "AP",
+    Ft = "FT",
+    NS = "NS",
+}
+
+export interface T1 {
+    Nm:       string;
+    ID:       string;
+    Abr:      string;
+    tbd:      number;
+    Gd:       number;
+    Pids:     { [key: string]: string[] };
+    CoNm:     string; // Country Name
+    CoId:     string; // Country ID (USA, PER)
+}
+
+
+export interface SeriesInfo {
+    totalLegs:     number;
+    currentLeg:    number;
+    aggScoreTeam1: number;
+    aggScoreTeam2: number;
+}
